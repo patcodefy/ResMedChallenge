6ea8a56a9f2c5c944cc0fb294a0053e59a1c58d0
Patrick Iradukunda <irapatu44@gmail.com>
2021-11-13 12:31:17 -0800

    Updated gitignore

--- ResMedChallenge/ResMedChallenge/Extensions/Date.swift
+++ ResMedChallenge/ResMedChallenge/Extensions/Date.swift
@@ -8,7 +8,7 @@
 import Foundation
 extension Date {
 
-    func toString() -> String? {
+    func toString() -> String {
         let dateFormatter = DateFormatter()
 
         dateFormatter.dateFormat = "yyyy-MM-dd"
